03083d95668c7dc02a448fff04efa0c4fb38e325
matthewskillington <matthew.skillington@ideagenplc.com>
2018-02-07 10:10:28 +0000

    added custom validation

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,11 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidatorFn } from '@angular/forms';
+import { AbstractControl } from '@angular/forms/src/model';
+
+function powerRange(min: number, max: number): ValidatorFn {
+    return (c: AbstractControl): {[key: string]: boolean } | null => {
+        if(c.value != undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+            return {'range': true};
+        }
+        return null;
+    };
+}
 
 @Component({
     selector: 'add-form',
     templateUrl: './form.component.html',
     styleUrls: ['./form.component.css']
 })
+
 
 export class AddFormComponent implements OnInit {
     carForm: FormGroup;
@@ -16,16 +27,16 @@ export class AddFormComponent implements OnInit {
 
     ngOnInit(): void {
         this.carForm = this.fb.group({
-            manufacturer: '',
-            model: '',
-            power: '',
-            price: '',
-            image: ''
+            manufacturer: ['', Validators.required, ],
+            model: ['', Validators.required],
+            power: ['', [Validators.required, powerRange(10, 1000)]],
+            price: ['', Validators.required],
+            image: ['']
         });
     }
 
     save() {
         console.log(this.carForm);
-        console.log('Saved:' + JSON.stringify(this.carForm.value))
+        console.log('Saved:' + JSON.stringify(this.carForm.value));
     }
 }
